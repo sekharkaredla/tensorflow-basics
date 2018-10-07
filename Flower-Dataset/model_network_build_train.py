1259ad841d412aa69062b6d7cd40f0edc8ad4faf
sekharkaredla <sekhar.karedla@gmail.com>
2018-10-07 12:31:17 -0700

    flower data

--- Flower-Dataset/model_network_build_train.py
+++ Flower-Dataset/model_network_build_train.py
@@ -1,11 +1,16 @@
+# refer https://cv-tricks.com/tensorflow-tutorial/training-convolutional-neural-network-for-image-classification/
+
 import tensorflow as tf
 from cnn import CNN
 from flatten_network import flatten
 from fully_connected_layer import create_fully_connected_layer
+from read_data import split_dataset_with_ratio
 IMAGE_HEIGHT = 256
 IMAGE_WIDTH = 256
 FILTERS = 3
 NO_OF_FLOWERS = 5
+EPOCHS = 500
+
 
 input = tf.placeholder(tf.float32, shape=[None, IMAGE_HEIGHT,IMAGE_WIDTH,FILTERS])
 output_true = tf.placeholder(tf.float32, shape=[None, NO_OF_FLOWERS])
@@ -21,9 +26,31 @@ layer_flat = flatten(layer4)
 
 layer_fully_connected = create_fully_connected_layer(input=layer_flat,no_of_inputs=layer_flat.get_shape()[1:4].num_elements(),no_of_outputs=5)
 
+# fileWriter = tf.summary.FileWriter(".",sess.graph)
+print(layer_fully_connected)
+
+cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits= layer_fully_connected, labels= output_true))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost_function)
+
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-fileWriter = tf.summary.FileWriter(".",sess.graph)
-print(layer_fully_connected)
+
+data_train, labels_train, data_test, labels_test = split_dataset_with_ratio(0.20)
+print "dataset content details:"
+print len(data_train), len(labels_train), len(data_test), len(labels_test)
+
+for each_epoch in range(EPOCHS):
+    sess.run(optimizer, feed_dict={input: data_train, output_true: labels_train})
+    cost = sess.run(cost_function,feed_dict={input:data_train, output_true:labels_train})
+    correct_predictions = tf.equal(tf.argmax(layer_fully_connected,1),tf.argmax(output_true,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    pred_outputs = sess.run(layer_fully_connected,{input:data_test})
+    mean_square_error = tf.reduce_mean(tf.square(pred_outputs - labels_test))
+    mean_square_error_value = sess.run(mean_square_error)
+    accuracy_value = sess.run(accuracy, {input: data_test, output_true: labels_test})
+    print "epoch number : "+ str(each_epoch) + " - cost : "+ str(cost)+ " - mse : "+ str(mean_square_error_value)+" - accuracy : "+str(accuracy_value)
+
+saver = tf.train.Saver()
+save_path = saver.save(sess, "/home/sekhar/EXTRAS/tensorflow-basics/Flower-Dataset")
 sess.close()
---
+++ Flower-Dataset/read_data.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import glob
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import shuffle
+
+
+def one_hot_encode(labels):
+    # if it is 'label1' it will be represented as [1, 0, 0] in one_hot_encoder. At a time only one output is hot
+    n_labels = len(labels)
+    n_unique_labels = len(np.unique(labels))
+    one_hot_encode = np.zeros((n_labels,n_unique_labels))
+    one_hot_encode[range(n_labels),labels] = 1
+    return one_hot_encode
+
+def split_dataset_with_ratio(ratio):
+    data = []
+    labels = []
+    flower_data_folder = "/home/sekhar/EXTRAS/flowers-recognition/flowers_reshaped/"
+    for each_flower_image in glob.glob(flower_data_folder+"*.jpg"):
+        image_data = cv2.imread(each_flower_image)
+        image_label = each_flower_image.split('/')[-1].split('_')[0]
+        data.append(image_data)
+        labels.append(image_label)
+        print image_label
+        print image_data
+
+    data, labels = shuffle(data, labels, random_state=1)
+    labelEncoder = LabelEncoder()
+    labelEncoder.fit(labels)
+    labels = labelEncoder.transform(labels)
+    labels = one_hot_encode(labels=labels)
+    data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=ratio, random_state=42)
+    return data_train, labels_train, data_test, labels_test
